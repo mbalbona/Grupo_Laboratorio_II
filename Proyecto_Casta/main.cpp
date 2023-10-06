271b272bb14835d13e66facd108f8ea3a20d3e69
mbalbona <matu-2001@hotmail.com.ar>
2023-10-05 23:04:32 -0300

    Setup SDL nuevamente
    Intente compilar y no funcionaba el programa como lo habiamos dejado

--- Proyecto_Casta/main.cpp
+++ Proyecto_Casta/main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
 using namespace std;
 
 #include "Game.h"
 
+
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
+
+
+
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
